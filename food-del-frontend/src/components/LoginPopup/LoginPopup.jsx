abc05c286eeaa8780c98cb1f7f3f798477dfd096
kanako-110 <kanako11097@gmail.com>
2024-07-15 23:35:09 -0700

    ✨ Feat: signup login layout

---
+++ food-del-frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./LoginPopup.css";
+import { assets } from "@/assets/assets.js";
+
+const LoginPopup = ({ setShowLogin }) => {
+  const [currentState, setCurrentState] = useState("Login");
+
+  return (
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currentState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt="close"
+          />
+        </div>
+        <div className="login-popup-inputs">
+          {currentState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your name" required />
+          )}
+          <input type="email" placeholder="Your email" required />
+          <input type="password" placeholder="Password" required />
+        </div>
+        <button>{currentState === "Login" ? "Login" : "Create account"}</button>
+        <div className="login-popup-condition">
+          {/* use label */}
+          <input type="checkbox" />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+        {currentState === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrentState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrentState("Login")}>Login here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopup;
